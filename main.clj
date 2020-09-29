1a16a7f1db9885f3308eb971cbb57ddf33c5a331
Philip Mohun <philmohun@gmail.com>
2020-09-29 19:15:30 +0000

    Added additional examples for vectors

--- main.clj
+++ main.clj
@@ -1,68 +1,82 @@
-(require '[clojure.repl :refer :all]) ;; import namespace with additional functions
-;(require '[clojure.math]) ;; import math namespace
+;   Copyright (c) Phil Mohun. All rights reserved.
+;   The use and distribution terms for this software are covered by the MIT License 
+;   By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+;   You must not remove this notice, or any other, from this software.
 
-(dir clojure.repl) ;; list all functions in imported namespace
-(source dir) ;; let's check out the sourcecode of one of our new macros
+;; import namespaces
+(require '[clojure.repl :refer :all]) ;; import namespace with additional REPL functions
+(import '[clojure.math.numeric-tower])
+(import '[clojure.contrib.trace])
 
+(defn vector-components
+"Breaks a vector into it's respective scalar components along the x and y axis. Accepts a vector of length 2, with the magnitude in the first position and the angle from the x axis in the second. Assumes cartesian coordinates."
+[c]
+(let [mag (first c) 
+      theta (degrees2radians (second c))
+; return a vector with the x and y components, respectively
+      x (* mag (Math/cos theta)) 
+      y (* mag (Math/sin theta))]
+      [x y]))
 
-;"1) Define a function greet that takes no arguments and prints Hello. Replace the with the implementation: (defn greet [] )"
+(defn add-first [v]
+  (let [v (first v)] (recur (next v))))
 
-;(defn greet [] (print "hello"))
+(defn resultant-magnitude
+"Uses the component method to determine the magnitude of a resulant vector when given corresponding x and y components in a Cartesian coordinates system. "
+[c]
+(let [x (first c) 
+      y (second c)
+      R (Math/sqrt (+ (Math/pow x 2) (Math/pow y 2)))]
+      ; return the magnitude of the Resultant vector
+      [R]))
 
-; 2) Redefine greet using def, first with the fn special form and then with the #() reader macro.
+(defn resultant-direction
+"Uses the x and y components of a vector to determine it's direction within a Cartesian coordinate system"
+[c]
+(let [x (first c) 
+      y (second c)
+      theta (+ 180 (Math/toDegrees (Math/atan (/ y x))))]
+      ; return the angle of the Resultant vector with respect to the x-axis, in radians
+      [theta]))
 
-;(def greet (fn [] print "hello"))
-; (def greet #(print "hello"))
+(defn compute-resultant
+[c]
+(let [x (first (vector-components c))
+      y (second (vector-components c))
+      R (resultant-magnitude [x y])
+      theta (resultant-direction [x y])]
+      [R theta]))
 
-; 3) Define a function greeting which:
-;Given no arguments, returns "Hello, World!"
-;Given one argument x, returns "Hello, x!"
-;Given two arguments x and y, returns "x, y!"
+(defn test-fxn
+([c]
+(map vector-components c)))
+;(loop [i count (map results)] 
+;(while (>= i 0) 
+;(dec i) 
+;(map + results))))))
 
-;(defn conditional ([] (print "Hello, world!")) ([x] (print "Hello, " [x])) ([x y] (print [x] ", " [y] "!")))
+(defn multi-vector-resultant
+"Given two or more vectors, compute the Resulant vector. Assumes vectors contain two values, each with a magnitude and direction using Cartesian coordinates."
+([c1 c2] (let 
+  ;; Take each vector and break down into component vectors.
+  [x1 (first (vector-components c1))
+  y1 (second (vector-components c1))
+  x2 (first (vector-components c2))
+  y2 (second (vector-components c2))
+  ;; Add component vectors
+  x (+ x1 x2)
+  y (+ y1 y2)
+  ;; Calculate magnitude and direction of resultant and print as vector
+  R (resultant-magnitude [x y])
+  theta (resultant-direction [x y])]
+  [R theta]
+))
+([c1 c2 &args] (print "Please try again with two vectors.")))
 
-;; For testing
-;(assert (= "Hello, World!" (greeting)))
-;(assert (= "Hello, Clojure!" (greeting "Clojure")))
-;(assert (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))
+(defn foo [n] (fn [i] (+ i n)))
 
-;4) Define a function do-nothing which takes a single argument x and returns it, unchanged.
-
-(defn do-nothing [x] x)
-;(do-nothing 10)
-
-;5) Define a function always-thing which takes any number of arguments, ignores all of them, and returns the number 100.
-
-(defn always-thing [& x] (print "100"))
-;(always-thing)
-
-;6) Define a function make-thingy which takes a single argument x. It should return another function, which takes any number of arguments and always returns x.
-
-(defn make-thingy [x] (fn [& args] x))
-;((make-thingy 10))
-
-;7) Define a function triplicate which takes another function and calls it three times, without any arguments.
-
-(defn triplicate [f] (f)(f)(f))
-;(triplicate always-thing)
-
-;8) Define a function opposite which takes a single argument f. It should return another function which takes any number of arguments, applies f on them, and then calls not on the result. The not function in Clojure does logical negation.
-
-;; Define a clojure function that accepts an argument, adds ten, and prints the result.
-(defn add-ten [x] (+ x 10)) 
-
-(defn opposite [f] (fn [& args] (not (apply f args))))
-; ((opposite add-ten) 0)
-; ((opposite equals) 10)
-
-;; so look at this one "nested"
-;; trying to determine which order arguments are evaluated
-(defn nested [x] (fn [& args] (fn [& zyx] print zyx)))
-;;(((nested 10) 20) 30)
-
-;; write a function that, when given the magnitude and direction of a vector breaks it into components
-;(defn vector-components [mag theta] (* mag Math/cos theta) (mag Math/sin theta))
-
-;; write a function that calculates displacement of multiple vectors
-(defn calc-displacement [mag1 theta1 mag2 theta2] )
-;; calculate the x and y components of each vector)
+(defn Example []
+   (loop [x 10]
+      (when (> x 1)
+         (println x)
+         (recur (- x 2))))) 
